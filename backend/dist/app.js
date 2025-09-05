e1ce5896e370511bd7568e2174fba2cc2017c3d0
iamCoderMohit <mohitjoshiu@gmail.com>
2025-09-05 20:39:40 +0530

    init

---
+++ backend/dist/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    res.send("working");
+});
+export default app;
+//# sourceMappingURL=app.js.map
---
+++ backend/dist/server.js
@@ -0,0 +1,4 @@
+import app from "./app.js";
+const PORT = 3000;
+app.listen(PORT, () => console.log("app is listneing on port", PORT));
+//# sourceMappingURL=server.js.map

30fba00c3ff0f57f3907783d0b80108a06e82b4f
iamCoderMohit <mohitjoshiu@gmail.com>
2025-09-10 23:57:14 +0530

    fix: ui send button

--- frontend/src/components/OptionLayout.tsx
+++ frontend/src/components/OptionLayout.tsx
@@ -5,8 +5,8 @@ function OptionLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <FeedLayout>
-        <div className="h-[calc(100%-83px)] w-full md:w-[50%] absolute -translate-x-1/2 left-1/2 border border-t-0 border-b-0">
-          <div className=" h-full overflow-auto">{children}</div>
+        <div className="h-[calc(100%-83px)] w-full md:w-[50%] absolute -translate-x-1/2 left-1/2 border border-t-0 border-b-0 z-5">
+          <div className=" h-full overflow-auto z-[-10]">{children}</div>
           <BottomBar />
         </div>
       </FeedLayout>
